6e5f4eda580cd8c91b5f8946669dc7d62ed7c0b6
cedric-h <cedhut02@gmail.com>
2021-11-08 12:57:07 -0500

    MSAA with some abstractions to make resizing sensible

--- glsl/splash.vert
+++ glsl/splash.vert
@@ -1,12 +1,13 @@
 #version 300 es
-
 precision highp float;
 precision highp int;
 
-uniform mat4 MVP;
-
 in vec2 position;
+in vec2 texcoord;
+
+out vec2 uv;
 
 void main() {
-  gl_Position = MVP * vec4(position, 0.0, 1.0);
+  uv = texcoord;
+  gl_Position = vec4(position, 0.0, 1.0);
 }
--- main.ts
+++ main.ts
@@ -1,41 +1,245 @@
 import defaultVertSrc from "./glsl/default.vert"
 import defaultFragSrc from "./glsl/default.frag"
-import fullscreenVertSrc from "./glsl/fullscreen.vert"
-import fullscreenFragSrc from "./glsl/fullscreen.frag"
+import splashVertSrc from "./glsl/splash.vert"
+import splashFragSrc from "./glsl/splash.frag"
 import fernGeoJSON from "./blender/fern.json"
-import { mat4, vec3 } from "gl-matrix";
+import { mat4 } from "gl-matrix";
 type WebGLCtx = WebGL2RenderingContextStrict;
 import ArrayType = WebGLRenderingContextStrict.ArrayType;
 import ShaderType = WebGLRenderingContextStrict.ShaderType;
 
-function createProgram(gl: WebGLCtx, vertexShaderSource: string, fragmentShaderSource: string) {
-  function createShader(gl: WebGLCtx, source: string, kind: ShaderType) {
-    const shader = gl.createShader(kind)!;
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    return shader;
+abstract class Buffer {
+  buf: WebGLBuffer;
+  abstract target(gl: WebGLCtx): WebGL2RenderingContextStrict.BufferTarget;
+
+  constructor(
+    gl: WebGLCtx,
+    usage: WebGL2RenderingContextStrict.BufferDataUsage,
+    public data: Float32Array | Uint16Array | Uint8Array,
+  ) {
+    this.buf = gl.createBuffer()!;
+    gl.bindBuffer(this.target(gl), this.buf);
+    gl.bufferData(this.target(gl), data, usage);
+    gl.bindBuffer(this.target(gl), null);
   }
 
-  const program = gl.createProgram()!;
-  const vshader = createShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
-  const fshader = createShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
-  gl.attachShader(program, vshader);
-  gl.deleteShader(vshader);
-  gl.attachShader(program, fshader);
-  gl.deleteShader(fshader);
-  gl.linkProgram(program);
+  bind(gl: WebGLCtx) { gl.bindBuffer(this.target(gl), this.buf); }
+  unbind(gl: WebGLCtx) { gl.bindBuffer(this.target(gl), null); }
+}
+class VertexBuffer extends Buffer {
+  target(gl: WebGLCtx) { return gl.ARRAY_BUFFER }
+}
+class IndexBuffer extends Buffer {
+  target(gl: WebGLCtx) { return gl.ELEMENT_ARRAY_BUFFER }
+}
 
-  let log = gl.getProgramInfoLog(program);
-  if (log) console.log(log);
+type UniformDesc = { name: string; kind: string; }
+type FragStageDesc = { src: string; uniforms: UniformDesc[] };
+type AttributeDesc = {
+  name: string;
+  size: 1 | 2 | 3 | 4;
+  kind: ArrayType;
+  normalized?: GLboolean;
+  stride?: GLsizei;
+  offset?: GLintptr;
+  instanced?: number
+}
+type VertStageDesc = FragStageDesc & { attributes: AttributeDesc[] };
+type ShaderPairDesc = { vert: VertStageDesc, frag: FragStageDesc };
+class ShaderPair {
+  program: WebGLProgram;
+  uniforms: Map<string, { loc: WebGLUniformLocation, kind: string }>;
+  attrs: (AttributeDesc & { loc: number })[];
+  constructor(gl: WebGLCtx, desc: ShaderPairDesc) {
+    function createShader(kind: ShaderType, src: string) {
+      const shdr = gl.createShader(kind)!;
+      gl.shaderSource(shdr, src);
+      gl.compileShader(shdr);
+      if (gl.getShaderParameter(shdr, gl.COMPILE_STATUS))
+        return shdr;
 
-  log = gl.getShaderInfoLog(vshader);
-  if (log) console.log(log);
+      const info = gl.getShaderInfoLog(shdr);
+      gl.deleteShader(shdr);
+      const shdrKindName = ["VERTEX_SHADER", "FRAGMENT_SHADER"].find(x => (gl as any)[x] == kind);
+      throw new Error("Couldn't compile " + shdrKindName + ": " + info);
+    }
 
-  log = gl.getShaderInfoLog(fshader);
-  if (log) console.log(log);
+    function createProgram(vert: WebGLShader, frag: WebGLShader) {
+      const program = gl.createProgram()!;
+      gl.attachShader(program, vert);
+      gl.attachShader(program, frag);
+      gl.linkProgram(program);
+      if (gl.getProgramParameter(program, gl.LINK_STATUS))
+        return program;
+     
+      const info = gl.getProgramInfoLog(program);
+      gl.deleteProgram(program);
+      throw new Error("" + info);
+    }
 
-  return program;
-};
+    this.program = createProgram(
+      createShader(gl.VERTEX_SHADER, desc.vert.src),
+      createShader(gl.FRAGMENT_SHADER, desc.frag.src)
+    );
+    gl.useProgram(this.program);
+    this.uniforms = new Map([desc.frag.uniforms, desc.vert.uniforms].flat().map(u => {
+      return [u.name, { loc: gl.getUniformLocation(this.program, u.name)!, kind: u.kind }];
+    }));
+    this.attrs = desc.vert.attributes.map(a => Object.assign({
+      loc: gl.getAttribLocation(this.program, a.name)
+    }, a));
+  }
+
+  makeVAO(gl: WebGLCtx, bind: Map<string, Buffer>): WebGLVertexArrayObject {
+    const vao = gl.createVertexArray()!;
+    gl.bindVertexArray(vao);
+    gl.useProgram(this.program);
+    for (const { name, loc, size, kind, normalized, stride, offset, instanced } of this.attrs) {
+      bind.get(name)!.bind(gl);
+      gl.enableVertexAttribArray(loc);
+      gl.vertexAttribPointer(loc, size, kind, normalized ?? false, stride ?? 0, offset ?? 0);
+      if (typeof instanced == "number")
+        gl.vertexAttribDivisor(loc, instanced);
+    }
+    gl.bindVertexArray(null);
+    return vao;
+  }
+
+  use(gl: WebGLCtx) {
+    gl.useProgram(this.program);
+  }
+}
+
+class Geometry {
+  constructor(public vao: WebGLVertexArrayObject, public count: number) {}
+  draw(gl: WebGLCtx) {
+    gl.bindVertexArray(this.vao);
+    gl.drawArrays(gl.TRIANGLES, 0, this.count);
+    gl.bindVertexArray(null);
+  }
+}
+
+class IndexedGeometry {
+  constructor(public vao: WebGLVertexArrayObject, public indices: IndexBuffer) {}
+  draw(gl: WebGLCtx) { 
+    gl.bindVertexArray(this.vao);
+    this.indices.bind(gl);
+    gl.drawElements(gl.TRIANGLES, this.indices.data.length, gl.UNSIGNED_SHORT, 0);
+    this.indices.unbind(gl);
+    gl.bindVertexArray(null);
+  }
+}
+
+/* one FRAMEbuffer with MSAA enabled, then another with a custom RENDERbuffer so that it can be
+ * sampled as a texture for rendering onto a fullscreen quad.
+ */
+class PassMSAA {
+  renderFrame: WebGLFramebuffer;
+  colorFrame: WebGLFramebuffer;
+  colorRenderbuffer: WebGLRenderbuffer;
+  colorTexture: WebGLTexture;
+  shaders: ShaderPair;
+  fullscreenQuad: Geometry;
+  constructor(gl: WebGLCtx, public width: number, public height: number) {
+    const texture = this.colorTexture = gl.createTexture()!;
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+    // -- Init Frame Buffers
+    const colorFrame = this.colorFrame = gl.createFramebuffer()!;
+    const renderFrame = this.renderFrame = gl.createFramebuffer()!;
+    const colorRenderbuffer = this.colorRenderbuffer = gl.createRenderbuffer()!;
+    gl.bindRenderbuffer(gl.RENDERBUFFER, colorRenderbuffer);
+    gl.renderbufferStorageMultisample(gl.RENDERBUFFER, 8, gl.RGBA8, width, height);
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, renderFrame);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, colorRenderbuffer);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, colorFrame);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    this.shaders = new ShaderPair(gl, {
+      vert: {
+        src: splashVertSrc,
+        uniforms: [{ name: "diffuse", kind: "1i" }],
+        attributes: [
+          { name: "position", size: 2, kind: gl.FLOAT },
+          { name: "texcoord", size: 2, kind: gl.FLOAT },
+        ]
+      },
+      frag: { src: splashFragSrc, uniforms: [] }
+    });
+
+    this.fullscreenQuad = new Geometry(
+      this.shaders.makeVAO(gl, new Map([
+        ["position", new VertexBuffer(gl, gl.STATIC_DRAW, new Float32Array([
+          -1.0, -1.0,
+           1.0, -1.0,
+           1.0,  1.0,
+           1.0,  1.0,
+          -1.0,  1.0,
+          -1.0, -1.0
+        ]))],
+        ["texcoord", new VertexBuffer(gl, gl.STATIC_DRAW, new Float32Array([
+          0.0, 1.0,
+          1.0, 1.0,
+          1.0, 0.0,
+          1.0, 0.0,
+          0.0, 0.0,
+          0.0, 1.0
+        ]))]
+      ])),
+      6
+    );
+  }
+
+  delete(gl: WebGLCtx) {
+    gl.deleteTexture(this.colorTexture);
+    gl.deleteRenderbuffer(this.colorRenderbuffer);
+    gl.deleteFramebuffer(this.renderFrame);
+    gl.deleteFramebuffer(this.colorFrame);
+  }
+
+  startMSAA(gl: WebGLCtx) {
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.renderFrame);
+    gl.clearBufferfv(gl.COLOR, 0, [0.0, 0.0, 0.0, 1.0]);
+  }
+
+  showMSAA(gl: WebGLCtx) {
+    // Blit framebuffers, no Multisample texture 2d in WebGL 2
+    gl.bindFramebuffer(gl.READ_FRAMEBUFFER, this.renderFrame);
+    gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, this.colorFrame);
+    gl.clearBufferfv(gl.COLOR, 0, [0.0, 0.0, 0.0, 1.0]);
+    gl.blitFramebuffer(
+      0, 0, this.width, this.height,
+      0, 0, this.width, this.height,
+      gl.COLOR_BUFFER_BIT, gl.NEAREST
+    );
+
+    // Pass 2
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    this.shaders.use(gl);
+    gl.uniform1i(this.shaders.uniforms.get("diffuse")!.loc, 0);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, this.colorTexture);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    this.fullscreenQuad.draw(gl);
+  }
+
+  withMSAA(gl: WebGLCtx, msaaRender: (gl: WebGLCtx) => void) {
+    this.startMSAA(gl);
+    msaaRender(gl);
+    this.showMSAA(gl);
+  }
+}
 
 window.onload = () => {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -52,176 +256,31 @@ window.onload = () => {
     return;
   }
 
-  // -- Init program
-  const PROGRAM = {
-    DEFAULT: 0,
-    SPLASH: 1,
-    MAX: 2
-  };
-
-  const programs = [
-    createProgram(gl,    defaultVertSrc,    defaultFragSrc),
-    createProgram(gl, fullscreenVertSrc, fullscreenFragSrc)
-  ];
-  const mvpLocation = gl.getUniformLocation(programs[PROGRAM.DEFAULT], 'MVP');
-  const diffuseLocation = gl.getUniformLocation(programs[PROGRAM.SPLASH], 'diffuse');
-
-  // -- Init primitive data
-  const vertex_data = new Float32Array(fernGeoJSON.mesh.vertices.flat());
-  const index_data = new Uint16Array(fernGeoJSON.mesh.indices);
-
-  // -- Init buffers
-  const vertexDataBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertex_data, gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  const indexDataBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexDataBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, index_data, gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-  const positions = new Float32Array([
-    -1.0, -1.0,
-    1.0, -1.0,
-    1.0,  1.0,
-    1.0,  1.0,
-    -1.0,  1.0,
-    -1.0, -1.0
-  ]);
-  const vertexPosBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  const texCoords = new Float32Array([
-    0.0, 1.0,
-    1.0, 1.0,
-    1.0, 0.0,
-    1.0, 0.0,
-    0.0, 0.0,
-    0.0, 1.0
-  ]);
-  const vertexTexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, texCoords, gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  // -- Init Texture
-  // used for draw framebuffer storage
-  const FRAMEBUFFER_SIZE = {
-    x: canvas.width,
-    y: canvas.height
-  };
-  const texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-
-  // -- Init Frame Buffers
-  const FRAMEBUFFER = {
-    RENDERBUFFER: 0,
-    COLORBUFFER: 1
-  };
-  const framebuffers = [
-    gl.createFramebuffer(),
-    gl.createFramebuffer()
-  ];
-  const colorRenderbuffer = gl.createRenderbuffer();
-  gl.bindRenderbuffer(gl.RENDERBUFFER, colorRenderbuffer);
-  gl.renderbufferStorageMultisample(gl.RENDERBUFFER, 4, gl.RGBA8, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y);
-
-  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[FRAMEBUFFER.RENDERBUFFER]);
-  gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, colorRenderbuffer);
-  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-
-  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[FRAMEBUFFER.COLORBUFFER]);
-  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
-  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-
-  // -- Init VertexArray
-  const vertexArrays = [
-    gl.createVertexArray(),
-    gl.createVertexArray()
-  ];
-
-  const vertexPosLocation = 0; // set with GLSL layout qualifier
-
-  gl.bindVertexArray(vertexArrays[PROGRAM.DEFAULT]);
-  gl.enableVertexAttribArray(vertexPosLocation);
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
-  gl.vertexAttribPointer(vertexPosLocation, 2, gl.FLOAT, false, 0, 0);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  gl.bindVertexArray(null);
-
-  gl.bindVertexArray(vertexArrays[PROGRAM.SPLASH]);
-
-  gl.enableVertexAttribArray(vertexPosLocation);
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer);
-  gl.vertexAttribPointer(vertexPosLocation, 2, gl.FLOAT, false, 0, 0);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  const vertexTexLocation = 1; // set with GLSL layout qualifier
-  gl.enableVertexAttribArray(vertexTexLocation);
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexBuffer);
-  gl.vertexAttribPointer(vertexTexLocation, 2, gl.FLOAT, false, 0, 0);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  gl.bindVertexArray(null);
-
-  // -- Render
-
-  // Pass 1
-  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[FRAMEBUFFER.RENDERBUFFER]);
-  gl.clearBufferfv(gl.COLOR, 0, [0.0, 0.0, 0.0, 1.0]);
-  gl.useProgram(programs[PROGRAM.DEFAULT]);
-  gl.bindVertexArray(vertexArrays[PROGRAM.DEFAULT]);
-
-  const IDENTITY = mat4.create();
-  const scaleVector3 = vec3.create();
-  vec3.set(scaleVector3, 0.5, 0.5, 0.5);
-  const mvp = mat4.create();
-  mat4.scale(mvp, IDENTITY, scaleVector3);
-
-  gl.uniformMatrix4fv(mvpLocation, false, mvp);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexDataBuffer);
-  gl.drawElements(gl.TRIANGLES, fernGeoJSON.mesh.indices.length, gl.UNSIGNED_SHORT, 0);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-  // Blit framebuffers, no Multisample texture 2d in WebGL 2
-  gl.bindFramebuffer(gl.READ_FRAMEBUFFER, framebuffers[FRAMEBUFFER.RENDERBUFFER]);
-  gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, framebuffers[FRAMEBUFFER.COLORBUFFER]);
-  gl.clearBufferfv(gl.COLOR, 0, [0.0, 0.0, 0.0, 1.0]);
-  gl.blitFramebuffer(
-    0, 0, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y,
-    0, 0, FRAMEBUFFER_SIZE.x, FRAMEBUFFER_SIZE.y,
-    gl.COLOR_BUFFER_BIT, gl.NEAREST
+  const defaultShaderPair = new ShaderPair(gl, {
+    vert: {
+      src: defaultVertSrc,
+      uniforms: [{ name: "MVP", kind: "Matrix4fv" }],
+      attributes: [{ name: "position", size: 2, kind: gl.FLOAT }],
+    },
+    frag: { src: defaultFragSrc, uniforms: [] }
+  });
+  const { vertices, indices } = fernGeoJSON.mesh;
+  const fernVertPoses = new VertexBuffer(gl, gl.STATIC_DRAW, new Float32Array(vertices.flat()));
+  const fern = new IndexedGeometry(
+    defaultShaderPair.makeVAO(gl, new Map([["position", fernVertPoses]])),
+    new IndexBuffer(gl, gl.STATIC_DRAW, new Uint16Array(indices))
   );
 
-  // Pass 2
-  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-  gl.useProgram(programs[PROGRAM.SPLASH]);
-  gl.uniform1i(diffuseLocation, 0);
-  gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.bindVertexArray(vertexArrays[PROGRAM.SPLASH]);
+  const pass = new PassMSAA(gl, canvas.width, canvas.height);
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  pass.withMSAA(gl, gl => {
+    defaultShaderPair.use(gl);
 
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
+    let { width: w, height: h } = canvas;
+    w /= 90; h /= 90;
+    const mvp = mat4.ortho(mat4.create(), -w/2, w/2, -h/2, h/2, -1, 1);
+    gl.uniformMatrix4fv(defaultShaderPair.uniforms.get("MVP")!.loc, false, mvp);
 
-  // -- Delete WebGL resources
-  gl.deleteBuffer(vertexPosBuffer);
-  gl.deleteBuffer(vertexTexBuffer);
-  gl.deleteTexture(texture);
-  gl.deleteRenderbuffer(colorRenderbuffer);
-  gl.deleteFramebuffer(framebuffers[FRAMEBUFFER.RENDERBUFFER]);
-  gl.deleteFramebuffer(framebuffers[FRAMEBUFFER.COLORBUFFER]);
-  gl.deleteVertexArray(vertexArrays[PROGRAM.DEFAULT]);
-  gl.deleteVertexArray(vertexArrays[PROGRAM.SPLASH]);
-  gl.deleteProgram(programs[PROGRAM.DEFAULT]);
-  gl.deleteProgram(programs[PROGRAM.SPLASH]);
+    fern.draw(gl);
+  });
 }
